d77299f9d690293e79e783a5e73a3626aae3f8a0
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-04-15 01:49:28 +0430

    first video finsh

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
+const container= document.querySelector(".blogs");
 const renderPosts= async () =>{
     let uri="http://localhost:300/posts";
+
     const res= await fetch(uri);
     const posts= await res.json();
 
+
+  
+
+
+
+
     let template="";
-    posts.forEach(post => {
-        template += "
+    posts.forEach(post =>{
+        template += `
         <section class="post">
         <h2>${post.title}</h2>
         <p><smal>${post.likes} likes </smal></p>
         <p>${post.body.slice(0,200)}</p>
-        <a href="./js/details.html">read more..</a>
-        </section> "
+        <a href="./js/details.html?id=${post.id}">read more...</a>
+        </section> 
+        `
     });
+
+    container.innerHTML =template;
 }
 
 
