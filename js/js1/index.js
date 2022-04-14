c8775ff2ba2d28662507326688e957006d55d5de
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-04-14 17:27:21 +0430

    not completed json

--- js/js1/index.js
+++ js/js1/index.js
@@ -1,7 +1,18 @@
 const renderPosts= async () =>{
-    let uri="./**";
+    let uri="http://localhost:300/posts";
     const res= await fetch(uri);
-    const postd= await res.json();
+    const posts= await res.json();
+
+    let template="";
+    posts.forEach(post => {
+        template += "
+        <section class="post">
+        <h2>${post.title}</h2>
+        <p><smal>${post.likes} likes </smal></p>
+        <p>${post.body.slice(0,200)}</p>
+        <a href="./js/details.html">read more..</a>
+        </section> "
+    });
 }
 
 
